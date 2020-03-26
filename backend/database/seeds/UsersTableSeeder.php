a6bea0bd3933f8db19c3db99f41cc686777dd700
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-03-26 16:00:06 -0400

    Seeding all types of user

--- backend/database/seeds/UsersTableSeeder.php
+++ backend/database/seeds/UsersTableSeeder.php
@@ -19,8 +19,27 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('123456'),
             'role' => 'Admin'
         ]);
+        factory(User::class)->create([
+            'name' => 'Maintenance',
+            'email' => 'rogerio@test.com',
+            'password' => bcrypt('123456'),
+            'role' => 'Maintenance'
+        ]);
+        factory(User::class)->create([
+            'name' => 'Tenant',
+            'email' => 'tenant@test.com',
+            'password' => bcrypt('123456'),
+            'role' => 'Tenant'
+        ]);
+
+        //Admin
         factory(User::class, 3)->create([
             'role' => 'Admin'
+        ]);
+
+        //Maintenance
+        factory(User::class, 3)->create([
+            'role' => 'Maintenance'
         ]);
 
         //Tenants
